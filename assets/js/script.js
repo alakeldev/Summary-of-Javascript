2cc01d412c81de078ad9c572d1b6397d4888b380
unknown <abdullah.k.alakel@gmail.com>
2023-06-05 14:35:20 +0200

    Add data type section and some notes before it

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,3 @@
-
 /*
 Javascript In Two Lines:
 "JS" is Scripting Language, it's working on client side (Browser) and server side too (BackEnd).
@@ -23,5 +22,47 @@ The Main Sections In JS:
 - BOM 
 - OOP
 */
+
+//------------------------------------------------------------------------------------------------------------------------
+/*
+- window.alert("Write here your Message");    "to show alert Message to user,,,,,using alert property inside window object" 
+- document.write("<h1>Hello <span>World</span></h1>");     "Creat new Element, Text,,,, inside the document object"
+
+- console.log("Hello World")  "Print Message on console,,,,,inside console object we used log property"  
+- console.error("Error");      "Print Error Message on console,,,,,inside console object we use error property"
+- console.table(["Alakel", "Abode", "Ghost"])     "Print the array with its indexes in console as a table"
+- console.log("%cHello %cWorld", "color: red; font-size: 40px", "color: blue; font-size: 20px")
+"To add styling from the console: we are useing 'Directive (%c)' before the world or sentence that we want to add style on it".
+
+- * Web API (console is related to Web API (Application Programming Interface) *
+- * ES6 (check this website to check the difference between the ES new version and old versions // http://es6-features.org/#Constants)
+*/
+//-------------------------------------------------------------------------------------------------------------------------
+
+
+/*/*//*/*//*/*//*
+
+Data Types:
+
+- String
+- Number
+- Array => Object
+- Object
+- Boolean
+- Undefined
+- null => Object
+
+// Important Operator and Function : typeof()
+console.log(typeof("Alakel Abdullah"))   OR   console.log(typeof "5000")  // string 
+console.log(typeof(5000))   OR   console.log(typeof 5000.55)  // number
+console.log(typeof([10, 15, 17]))   OR   console.log(typeof [10, 15, 17])  // array (=> Object)
+console.log(typeof(["Abdullah", "Alakel", "SOSO"]))   OR   console.log(typeof ["Abdullah", "Alakel", "SOSO"])  // array (=> Object)
+console.log(typeof({name: "Abdulah", age: 17, country: "Syr"}))   OR   console.log(typeof {name: "Abdulah", age: 17, country: "Syr"}) //object
+console.log(typeof(true))   OR   console.log(typeof false) // boolean
+console.log(typeof(undefined))   OR   console.log(typeof undefined) // undefined
+console.log(typeof(null))   OR   console.log(typeof null) // object
+
+*//*//*///*/*//*
+//-------------------------------------------------------------------------------------------------------------------------
 
 
