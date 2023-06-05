498214259c596b57d41261fe445d2bf60635897b
unknown <abdullah.k.alakel@gmail.com>
2023-06-05 13:14:07 +0200

    Creat index.html page and connect it with js file and add some starting comments inside script.js file

---
+++ assets/js/script.js
@@ -0,0 +1,27 @@
+
+/*
+Javascript In Two Lines:
+"JS" is Scripting Language, it's working on client side (Browser) and server side too (BackEnd).
+"JS" is to implement complex features on your webpage by making it interactive.
+*/
+
+/*
+With JS you can do: 
+- Dynamically Update Content
+- Manipulating HTML and CSS
+- Animate Images, Content and Creat Image Gallery
+- Manipulate and Validate Data
+- Control MultiMedia (Video, Audio,..)
+- Web Browser Games
+- Mobile Apps, PS Apps, Cross Apps (by using frameworks to make it working in all MAC, Windows, Linux)
+*/
+
+/*
+The Main Sections In JS:
+- Vanilla JS
+- DOM 
+- BOM 
+- OOP
+*/
+
+
