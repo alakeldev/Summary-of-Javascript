a20f27977396af2befffe86666aea8913abee068
Abdullah Alakel <130242989+alakeldev@users.noreply.github.com>
2024-11-10 16:10:10 +0100

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -2062,6 +2062,24 @@ for (let key in obj) {
 }
 // name Ali
 // age 65
+
+------------------------------------------------------------------------------------------------------------------------
+
+Nullish Coalescing
+There are situations where you want to apply a default value in case a variable is null or undefined (but only then). 
+To address this, the nullish coalescing operator ?? was introduced in 2020. 
+It returns the right-hand side operand only when the left-hand side operand is null or undefined. 
+Otherwise, the left-hand side operand is returned.
+
+let amount = null;
+amount = amount ?? 1;
+// => 1
+
+amount = 0;
+amount = amount ?? 1;
+// => 0
+
+
 *//*//*///*//*
 //-------------------------------------------------------------------------------------------------------------------------
 /*/*//*/*//*//*
